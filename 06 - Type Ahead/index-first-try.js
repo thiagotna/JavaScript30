f1267f0a70a72ca98ae2e477d1144fb73b8c8047
thiagotna <thiago.tnaraujo@gmail.com>
2025-06-11 18:53:10 -0300

    Challenge #6

---
+++ 06 - Type Ahead/index-first-try.js
@@ -0,0 +1,44 @@
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+const cities = [] //Array for storing data from fetch API call
+const input = document.querySelector('.search') // Gets input element
+const suggestionsList = document.querySelector('.suggestions') // gets list element
+const defaultListItems = Array.from(suggestionsList.children) // Saves defaults list elements into a variable to be used later
+const handleChange = (e) => {
+  const inputValue = e.target.value //gets input value
+  const filteredCities = cities.filter(
+    (obj) => obj.city.includes(inputValue) || obj.state.includes(inputValue),
+  ) // returns filtered array that checks array elements that contains the string pattern
+
+  //Check input value for emprty or not
+  if (inputValue) {
+    clearSuggestionList()
+    //Run events for each element data from the stored array
+    filteredCities.forEach((filteredCity) => {
+      const listItem = document.createElement('li') //Create a li element for each filtered element
+
+      listItem.innerHTML = `<span>${filteredCity.city}, ${filteredCity.state}</span><small>${filteredCity.population}</small>` // fill li element with fetched data content
+      suggestionsList.appendChild(listItem) // append li element with content to the suggestion list
+    })
+  } else {
+    //I input value is empty clears the suggestion list and append the default list items
+    clearSuggestionList()
+    defaultListItems.forEach((listItem) => {
+      suggestionsList.appendChild(listItem)
+    })
+  }
+}
+//Clears list back to the default state
+const clearSuggestionList = () => {
+  while (suggestionsList.firstChild) {
+    suggestionsList.removeChild(suggestionsList.firstChild)
+  }
+}
+
+//Fetches data and save it into an array
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => cities.push(...data))
+
+//Adds event listener to the input
+input.addEventListener('input', handleChange)
---
+++ 06 - Type Ahead/index.js
@@ -0,0 +1,67 @@
+const endpoint =
+  'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+const cities = [] //Array for storing data from fetch API call
+const input = document.querySelector('.search') // Gets input element
+const suggestionsList = document.querySelector('.suggestions') // gets list element
+const defaultListItems = Array.from(suggestionsList.children) // Saves defaults list elements into a variable to be used later
+
+const findMatches = (string, array) => {
+  const regex = new RegExp(string, 'gi')
+
+  return array.filter((item) => {
+    return Object.values(item).some((value) => {
+      return typeof value === 'string' && value.match(regex)
+    })
+  })
+}
+
+const displayMatches = (e) => {
+  const string = e.target.value
+
+  if (!string) {
+    clearSuggestionList()
+    defaultListItems.forEach((listItem) => {
+      suggestionsList.appendChild(listItem)
+    })
+    return
+  }
+
+  const matchArray = findMatches(string, cities)
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(string, 'gi')
+      const cityName = place.city.replace(
+        regex,
+        `<span class="hl">${string}</span>`,
+      )
+      const stateName = place.state.replace(
+        regex,
+        `<span class="hl">${string}</span>`,
+      )
+
+      return `
+        <li>
+          <span class="name">${cityName}, ${stateName} </span>
+          <span class="population">${numberWithDots(place.population)}</span>
+        </li>`
+    })
+    .join('')
+  suggestionsList.innerHTML = html
+}
+
+const clearSuggestionList = () => {
+  while (suggestionsList.firstChild) {
+    suggestionsList.removeChild(suggestionsList.firstChild)
+  }
+}
+
+const numberWithDots = (number) => {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+}
+
+fetch(endpoint)
+  .then((response) => response.json())
+  .then((data) => cities.push(...data))
+
+//Adds event listener to the input
+input.addEventListener('input', displayMatches)
